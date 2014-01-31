af7489a9ff3c0ab7ed74592ec74ec1f6c710c547
igoryen <igor.yentaltsev@gmail.com>
2014-01-31 15:45:04 -0500

    Paint activity2 blue

--- gen/com/example/first/R.java
+++ gen/com/example/first/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int blue=0x7f060001;
         public static final int red=0x7f060000;
     }
     public static final class dimen {
