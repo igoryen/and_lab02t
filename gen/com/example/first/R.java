99f7e0fecfa1a8e2c604bb7da65ff57d424a3847
igoryen <igor.yentaltsev@gmail.com>
2014-01-31 15:43:53 -0500

    Paint activity1 red

--- gen/com/example/first/R.java
+++ gen/com/example/first/R.java
@@ -10,6 +10,9 @@ package com.example.first;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int red=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -24,15 +27,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int button=0x7f080000;
+        public static final int action_settings=0x7f090001;
+        public static final int button=0x7f090000;
     }
     public static final class layout {
         public static final int activity_first=0x7f030000;
         public static final int secondary=0x7f030001;
     }
     public static final class menu {
-        public static final int first=0x7f070000;
+        public static final int first=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -62,10 +65,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
